3c19a95d7d0d6c585b5b313fbdf7c1418b8d689d
Le Tu Anh <letuanh8219932@gmail.com>
2016-07-12 11:30:40 +0700

    create form signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:danger] = t "message.error_norecord"
+      redirect_to signup_path
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "message.success.create_user"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ActiveRecord::Base
   has_many :activity
   has_many :lesson
   has_many :relationship
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: {maximum: 50},
+    uniqueness: {case_sensitive: true}
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}, on: :create
+  has_secure_password
+
 end
