b4d97ccdd8b21c6bbcaa4e59537d68762bfae604
Le Tu Anh <letuanh8219932@gmail.com>
2016-07-20 13:26:04 +0700

    create edit categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CategoriesController < ApplicationController
-  before_action :load_category, only: [:index, :destroy]
+  before_action :load_category, except: [:new, :create]
+  before_action :verify_admin
 
   def index
     @categories = Category.order(:name).paginate page: params[:page],
@@ -9,7 +10,30 @@ class Admin::CategoriesController < ApplicationController
   def show
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = t "admin.create_category"
+      redirect_to admin_categories_url
+    else
+      render :new
+    end
+  end
+
   def edit
+  end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = t "admin.update_category"
+      redirect_to admin_categories_url
+    else
+      render :edit
+    end
   end
 
   def destroy
