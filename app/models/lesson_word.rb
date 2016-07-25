b12b44cef5ca7946e56e676f525d0f2efc17bdea
Le Tu Anh <letuanh8219932@gmail.com>
2016-07-25 17:44:13 +0700

    Do_lesson

--- app/models/lesson_word.rb
+++ app/models/lesson_word.rb
@@ -2,4 +2,7 @@ class LessonWord < ActiveRecord::Base
   belongs_to :lesson
   belongs_to :word
   belongs_to :word_answer
+  scope :correct_answer, -> do
+    joins(:word_answer).where word_answers: {is_correct: true}
+  end
 end
